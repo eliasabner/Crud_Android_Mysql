fb2b98049c6983b5b5a52b999d0fe9e4615cc2de
Elias Abner <eliasabner@hotmail.com>
2019-10-14 21:18:37 -0300

    Adicionar arquivos de projeto.

---
+++ Crud_Android_Mysql/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Crud_Android_Mysql
+{
+    [Activity(Label = "Crud_Android_Mysql", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
